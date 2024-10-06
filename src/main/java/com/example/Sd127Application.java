8e961ce2e4edcbbcd4d08853af08a28b69b12fda
candzha <canxt12@gmail.com>
2024-10-06 21:56:02 +0700

    first commit

---
+++ src/main/java/com/example/Sd127Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sd127Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sd127Application.class, args);
+    }
+
+}
